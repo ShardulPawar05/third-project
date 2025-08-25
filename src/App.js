9e6698f6c568636bff8c5babb0da70dafd8c47c4
shardul.pawar <shardulpawar53@gamil.com>
2025-08-25 16:19:59 +0530

    program of object display in table with ternary oprators with balck and white condition

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { useState } from 'react';
 
 function App() {
 
-let student=[ {sr:"01",name:"Shardul",marks:"80"},
-                 {sr:"02",name:"Gaurav",marks:"85"},
-                 {sr:"03",name:"Shravni",marks:"90"},
-                 {sr:"04",name:"Trusha",marks:"95"},
-                 {sr:"05",name:"Rutuja",marks:"99"}
+let student=[ {sr:"01",name:"Shardul",marks:100},
+                 {sr:"02",name:"Gaurav",marks:85},
+                 {sr:"03",name:"Shravni",marks:100},
+                 {sr:"04",name:"Trusha",marks:95},
+                 {sr:"05",name:"Rutuja",marks:100}
 ]
 
 
@@ -23,14 +23,13 @@ let student=[ {sr:"01",name:"Shardul",marks:"80"},
       </tr>
     
         {student.map((singlkey)=>{
-          return<tr>
+          return<tr className={singlkey.marks===100&&'rowColor'} >
+           
           <td>{singlkey.sr}</td>
           <td>{singlkey.name}</td>
           <td>{singlkey.marks}</td>
+          </tr>
           
-          
-          
-         </tr> 
         })}
       
 
