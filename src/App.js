71684650fcbe9d1dc3aa8d90ac259b6fcd910ddc
shardul.pawar <shardulpawar53@gamil.com>
2025-08-25 15:31:23 +0530

    program of creating multiple object in array and show them in table form in screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-
-
-
-     let obj={name:"Shardul",STD:"TYBCS",CLG:"BJ"}
-
-    let out=console.log(obj)
-
-
+let student=[ {sr:"01",name:"Shardul",marks:"80"},
+                 {sr:"02",name:"Gaurav",marks:"85"},
+                 {sr:"03",name:"Shravni",marks:"90"},
+                 {sr:"04",name:"Trusha",marks:"95"},
+                 {sr:"05",name:"Rutuja",marks:"99"}
+]
 
 
 
   return (
-    <div>
-     
-    <p><h1>Name: {obj.name}</h1> <h2>CLG: {obj.CLG}</h2>
-      <h3>Std: {obj.STD} </h3>
-       
-{out}
-     </p>
-    </div>
+  <div>
+    <table border={1}>
+      <tr>
+      <th>SR.NO</th>
+      <th>Name</th>
+      <th>Marks</th>
+      </tr>
+    
+        {student.map((singlkey)=>{
+          return<tr>
+          <td>{singlkey.sr}</td>
+          <td>{singlkey.name}</td>
+          <td>{singlkey.marks}</td>
+          
+          
+          
+         </tr> 
+        })}
+      
+
+
+
+    </table>
+
+
+  </div>
   );
 }
 
